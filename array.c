d4ee8ae0ced688a91582c6d9a5e18f53143773d5
Nguruko251 <nguruko.21531@students.kyu.ac.ke>
2024-10-15 07:48:42 +0300

    Add files via upload

---
+++ array.c
@@ -0,0 +1,11 @@
+//Arays in C
+#include<stdio.h>
+int main(){
+    int marks[5]={50,90,60,70,40};
+    printf("%d \n",marks[0]);
+    printf("%d \n",marks[1]);
+    printf("%d \n",marks[2]);
+    
+    return 0;
+           
+}
